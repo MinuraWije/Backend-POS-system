431f1fa0310eed66e2927c705e9233f7a542a9a5
MinuraWije <minurawije@gmail.com>
2024-08-28 22:36:09 +0530

    renamed to Order

--- src/main/java/org/example/backendpossystem/dto/Order.java
+++ src/main/java/org/example/backendpossystem/dto/Order.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class OrderDto {
+public class Order {
     private String orderId;
     private String customerId;
     private String customerName;
