cb9659549926435caabebca1c2765b828841fe2d
MinuraWije <minurawije@gmail.com>
2024-08-28 19:47:21 +0530

    Initial commit

---
+++ src/main/java/org/example/backendpossystem/db.sql
@@ -0,0 +1,7 @@
+create table customer(
+                        customerId varchar(255),
+                        customerName varchar(255),
+                        customerEmail varchar(255),
+                        customerAddress varchar(255),
+                        customerPhone varchar(255)
+);
